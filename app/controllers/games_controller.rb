de536f0afa0e15113af1d4f139d42f8fc0e39d09
Reinaldo Sanchez <sreinaldo@gmail.com>
2019-11-30 09:46:33 -0400

    show game board

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,7 @@
+class GamesController < ApplicationControllerGame
+  def index
+    @game_board = GameBoard.find(16)
+    @game_rows = [*0..@game_board.rows - 1]
+    @game_board_cells = @game_board.cells.by_row_column
+  end
+end
