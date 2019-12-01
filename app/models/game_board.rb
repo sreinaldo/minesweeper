0633a9fcc0e36b8a02d6664fc3aaa5372e1690e9
Reinaldo Sanchez <sreinaldo@gmail.com>
2019-12-01 03:38:16 -0400

    add method to check game status

--- app/models/game_board.rb
+++ app/models/game_board.rb
@@ -4,65 +4,85 @@ class GameBoard < ApplicationRecord
 
   after_create :create_board
 
+  enum status: {in_progress: 0, winner: 1, game_over: 2}
 
-  def create_board
-    cells = init_board(self.rows, self.columns)
-    cells = add_mines(self.rows, self.columns, self.mines, cells)
-    print(cells)
-    create_cells(cells, self.id)
-  end
-
-  def init_board(rows, cols)
-    cells = Array.new(rows) {Array.new(cols, 0)}
-    rows.times do |x|
-      cols.times do |y|
-        cells[x][y] = 0
-      end
+  def self.refresh_status(cell)
+    if cell.value == 9 && cell.opened?
+      cell.game_board.status = :game_over
+      cell.game_board.end_at = Time.now
+      cell.game_board.save
+      return nil
     end
 
-    cells
+    total_opened_cells = cell.game_board.cells.opened.count
+    total_game_cells = cell.game_board.rows * cell.game_board.columns
+    total_mines = cell.game_board.mines
+    if total_opened_cells + total_mines == total_game_cells
+      cell.game_board.status = :winner
+      cell.game_board.end_at = Time.now
+      cell.game_board.save
   end
+end
 
-  def add_mines(rows, cols, mines, cells)
-    mines.times do
-      col = row = 0
-      loop do
-        col = rand(cols)
-        row = rand(rows)
-        break if cells[row][col] != 9
-      end
-      cells[row][col] = 9
-      cells = add_number_around_mine(row, col, rows, cols, cells)
-    end
+def create_board
+  board = init_board(self.rows, self.columns)
+  board = add_mines(self.rows, self.columns, self.mines, board)
+  create_cells(board, self.id)
+end
 
-    cells
-  end
-
-  def add_number_around_mine(mine_row, mine_col, max_row, max_col, cells)
-    x = [0, mine_row - 1].max
-    while x <= [max_row - 1, mine_row + 1].min
-      y = [0, mine_col - 1].max
-      while y <= [max_col - 1, mine_col + 1].min
-        if cells[x][y] != 9
-          cells[x][y] += 1
-        end
-        y += 1
-      end
-      x += 1
-    end
-
-    cells
-  end
-
-  def create_cells(cells, game_board_id)
-    cells.each_with_index do |item, row|
-      cells[row].each_with_index do |item, col|
-        Cell.create!(row: row,
-                     column: col,
-                     status: :close,
-                     value: cells[row][col],
-                     game_board_id: game_board_id)
-      end
+def init_board(rows, cols)
+  cells = Array.new(rows) {Array.new(cols, 0)}
+  rows.times do |x|
+    cols.times do |y|
+      cells[x][y] = 0
     end
   end
+
+  cells
+end
+
+def add_mines(rows, cols, mines, cells)
+  mines.times do
+    col = row = 0
+    loop do
+      col = rand(cols)
+      row = rand(rows)
+      break if cells[row][col] != 9
+    end
+    cells[row][col] = 9
+    cells = add_number_around_mine(row, col, rows, cols, cells)
+  end
+
+  cells
+end
+
+def add_number_around_mine(mine_row, mine_col, max_row, max_col, cells)
+  x = [0, mine_row - 1].max
+  while x <= [max_row - 1, mine_row + 1].min
+    y = [0, mine_col - 1].max
+    while y <= [max_col - 1, mine_col + 1].min
+      if cells[x][y] != 9
+        cells[x][y] += 1
+      end
+      y += 1
+    end
+    x += 1
+  end
+
+  cells
+end
+
+def create_cells(cells, game_board_id)
+  cells.each_with_index do |item, row|
+    cells[row].each_with_index do |item, col|
+      Cell.create!(row: row,
+                   column: col,
+                   status: :closed,
+                   value: cells[row][col],
+                   game_board_id: game_board_id)
+    end
+  end
+end
+
+
 end
